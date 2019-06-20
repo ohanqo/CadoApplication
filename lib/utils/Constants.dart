907ec54a0538b54ea8d97a151ccb0e1e0f5c2787
Adam Antoine <antoine.adam@u-psud.fr>
2019-06-20 21:10:11 +0200

    Feature/login-api

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -2,5 +2,8 @@ class Constants {
   static const API_BASE_URL = 'https://api.lyberteam.eu';
   static const REQUEST_OK_CODE = 200;
   static const REQUEST_CREATED_CODE = 201;
+  static const REQUEST_LOGIN_OK_CODE = 202;
+  static const REQUEST_NOT_FOUND_CODE = 404;
+  static const REQUEST_UNAUTHORIZED_CODE = 401;
   static const REQUEST_USERNAME_ALREADY_EXIST_CODE = 226;
 }
